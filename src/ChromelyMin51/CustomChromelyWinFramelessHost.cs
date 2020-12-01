3745152d86a87a5876f4b9ca1e6aaa0c780d5184
Pavel Mikheev <pavel.mikheev@emu.codes>
2020-12-01 13:07:26 +0700

    Change native host Window styles.
    To reproduce: https://github.com/chromelyapps/Chromely/issues/264

--- src/ChromelyMin51/CustomChromelyWinFramelessHost.cs
+++ src/ChromelyMin51/CustomChromelyWinFramelessHost.cs
@@ -1,8 +1,17 @@
+using Chromely.Core.Host;
 using Chromely.NativeHost;
+using WS = Chromely.Interop.User32.WS;
 
 namespace ChromelyMin51
 {
     public class CustomChromelyWinFramelessHost : ChromelyWinFramelessHost
     {
+        protected override WindowStylePlacement GetWindowStylePlacement(WindowState state)
+        {
+            var placement = base.GetWindowStylePlacement(state);
+            placement.Styles = WS.POPUPWINDOW | WS.CLIPCHILDREN | WS.CLIPSIBLINGS | WS.SIZEBOX | WS.MINIMIZEBOX | WS.MAXIMIZEBOX;
+
+            return placement;
+        }
     }
 }
